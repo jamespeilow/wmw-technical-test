7e2a8b30a5f16f7f21f291e5f0fb57183c3dfca5
James Peilow <james.peilow@live.co.uk>
2018-10-20 18:33:06 +0100

    Responsive design added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
   const triggers = document.querySelectorAll('.dropdown');
+  const menuTogglers = document.querySelectorAll('.menu-toggle');
+  const menu = document.querySelector('.main-nav');
+  function toggleMenu(e) {
+    e.preventDefault();
+    menu.classList.toggle('menu-open');
+  }
 
   function handleEnter(e) {
-    console.log('hover');
     this.classList.add('trigger-enter');
     setTimeout(() => {
       if (this.classList.contains('trigger-enter')) {
@@ -9,9 +14,9 @@
       }}, 150);
   }
   function handleLeave(e) {
-    console.log('off');
     this.classList.remove('trigger-enter');
     this.classList.remove('trigger-enter-active');
   }
   triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
   triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
+  menuTogglers.forEach(menuToggle => menuToggle.addEventListener('click', toggleMenu));
